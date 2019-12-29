63d9779c4a445761d499a6bf5c9573323d707bc9
James Stanton <jestanton@outlook.com>
2019-12-29 15:14:10 +0000

    Moved error marshalling to member function

--- internal/errors/errors.go
+++ internal/errors/errors.go
@@ -1,13 +1,21 @@
 package errors
 
+import "encoding/json"
+
+// B2ErrorCode is a typedef for uint16, used for the enumerations of error codes
+type B2ErrorCode = uint16
+
 // B2Error is a container for error data
 type B2Error struct {
 	Code    B2ErrorCode `json:"code"`
 	Message string      `json:"message"`
 }
 
-// B2ErrorCode is a typedef for uint16, used for the enumerations of error codes
-type B2ErrorCode = uint16
+// ToJSON returns a json format string of owning B2Error
+func (e B2Error) ToJSON() (jsonMessageBody string) {
+	jsonMessage, _ := json.Marshal(e)
+	return string(jsonMessage)
+}
 
 // None is used when there is no error
 const None B2ErrorCode = iota
