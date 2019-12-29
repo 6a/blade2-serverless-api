c600edb1413ffebac290a18c70a8a686d024a32d
James Stanton <jestanton@outlook.com>
2019-12-29 15:13:04 +0000

    tweaked build script post go reinstall

--- build.ps1
+++ build.ps1
@@ -1,5 +1,6 @@
 param([string]$function="")
 
+# Only run if an arg was provided
 if ($function -eq "")
 {
     Write-Host "`n[ERROR] Please provide the function name`n" -ForegroundColor Red;
@@ -8,12 +9,17 @@ else
 {
     Write-Host "`n[INFO] Attempting to create lambda package '$function'" -ForegroundColor Blue;
 
-    $env:GOOS = "linux"; 
+    # Environment
+    $env:GOOS = "linux"
+    $env:GOARCH = "amd64"
+    $env:CGO_ENABLED = "0"
+
+    # Vars
     $binary = "$function.bin"
     $zip = "$function.zip"
     $entrypoint = "./internal/endpoints/$function.go"
 
-
+    # Build go binary
     go build -o $binary $entrypoint; 
 
     if ($? -ne $true) {
@@ -21,7 +27,8 @@ else
         exit
     }
     
-    build-lambda-zip.exe --output $zip $binary;
+    # Build aws lambda zip
+    build-lambda-zip --output $zip $binary;
 
     if ($? -ne $true) {
         Write-Host "[ERROR] Error while building AWS lambda package $zip'`n" -ForegroundColor Red;
