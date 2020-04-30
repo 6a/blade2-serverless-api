03cb66c3681142b6355e2a8b49d3817a61918015
James Stanton <jestanton@outlook.com>
2020-04-30 09:05:52 +0100

    tweaked player type to be more intuitive

--- pkg/elo/player.go
+++ pkg/elo/player.go
@@ -1,11 +1,11 @@
 package elo
 
 // Player is a typedef for the two different possible players (player 1 or player 2)
-type Player int
+type Player uint8
 
 // Winner type enums
 const (
-	Player1 Player = iota
-	Player2 Player = iota
-	Draw    Player = iota
+	Draw Player = iota
+	Player1
+	Player2
 )
