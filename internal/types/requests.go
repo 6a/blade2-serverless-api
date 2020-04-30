69f20fd55420d1e80303b04044565a6851624c15
James Stanton <jestanton@outlook.com>
2020-04-30 09:06:05 +0100

    adding framework for mmr update api call

--- internal/types/requests.go
+++ internal/types/requests.go
@@ -1,8 +1,17 @@
 package types
 
+import "github.com/6a/blade-ii-api/pkg/elo"
+
 // UserCreationRequest describes the data needed to create a new user
 type UserCreationRequest struct {
 	Handle   *string `json:"handle"`
 	Email    *string `json:"email"`
 	Password *string `json:"password"`
+}
+
+// MMRUpdateRequest describes the data needed to update the MMR for a pair of users after a match
+type MMRUpdateRequest struct {
+	Player1ID *uint64
+	Player2ID *uint64
+	Winner    *elo.Player
 }
