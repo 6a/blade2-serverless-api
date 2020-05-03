ea9df1eb2d48d2dc6a2d221c3eaef4fd2ac6bcc1
James Stanton <jestanton@outlook.com>
2020-05-03 15:19:06 +0100

    added json formatting markup things

--- internal/types/requests.go
+++ internal/types/requests.go
@@ -11,7 +11,7 @@ type UserCreationRequest struct {
 
 // MMRUpdateRequest describes the data needed to update the MMR for a pair of users after a match
 type MMRUpdateRequest struct {
-	Player1ID *uint64
-	Player2ID *uint64
-	Winner    *elo.Player
+	Player1ID *uint64     `json:"player1id"`
+	Player2ID *uint64     `json:"player2id"`
+	Winner    *elo.Player `json:"winner"`
 }
