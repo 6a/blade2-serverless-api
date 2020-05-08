9ff81a3387510a18f717d4a37fbcce4b6c9758e6
James Stanton <jestanton@outlook.com>
2020-05-08 03:38:01 +0100

    auth package comments and cleanup

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -1,3 +1,9 @@
+// Copyright 2020 James Einosuke Stanton. All rights reserved.
+// Use of this source code is governed by the MIT license
+// that can be found in the LICENSE.md file.
+
+// Package auth implements provides a helper function for extracting the username and password
+// from a Basic Authorization header as specified by (RFC7617).
 package auth
 
 import (
@@ -6,35 +12,42 @@ import (
 	"strings"
 )
 
-// ExtractCredentials attempts to extract and decode the Authorization header in a set of request headers
+const (
+
+	// basicAuthHeaderDelimiter as specified by (RFC7617).
+	basicAuthHeaderDelimiter = ":"
+
+	// credentialsArrayExpectedSize is the epected size of the Basic Auth header after being decoded and split.
+	credentialsArrayExpectedSize = 2
+)
+
+// ExtractCredentials attempts to extract and decode the Basic Authorization header in a set of request headers,
+// as specified by (RFC7617).
 func ExtractCredentials(headers map[string]string) (handle string, password string, err error) {
+
+	// Check for the existence of the authorization header.
 	if authHeader, ok := headers["Authorization"]; ok {
+
+		// Attempt to decode the contents of the basic auth header. This involves removing the suffix "Basic "
+		// and then decoding the remainining string using standard base64 decoding.
 		decodedHeader, err := base64.StdEncoding.DecodeString(strings.Replace(authHeader, "Basic ", "", 1))
 
+		// An error indicates that the format of the authorization header was invalid, and so an error is returned.
 		if err != nil {
 			return "", "", errors.New("Authorization header could not be decoded as a base64 (standard) string")
 		}
 
-		var credentials = strings.Split(string(decodedHeader), ":")
-		if len(credentials) != 2 {
+		// The decoded string is split into an array. If the length of the resultant array is not
+		// (credentialsArrayExpectedSize), an error is returned.
+		var credentials = strings.Split(string(decodedHeader), basicAuthHeaderDelimiter)
+		if len(credentials) != credentialsArrayExpectedSize {
 			return "", "", errors.New("Authorization header format invalid")
 		}
 
-		handle, password := credentials[0], credentials[1]
-
-		return handle, password, nil
+		// Return the parsed credentials.
+		return credentials[0], credentials[1], nil
 	}
 
+	// If the authorization header was not found, exit with an error.
 	return "", "", errors.New("Authorization header not found")
 }
-
-// // ValidatePathAndHandle checks if the path parameter matches the expected handle
-// func ValidatePathAndHandle(authHandle string, pathHandle string) (err error) {
-// 	matches := authHandle == pathHandle
-
-// 	if !matches {
-// 		err = errors.New("The handle specified in the Authorization header does not match the handle specified in the path")
-// 	}
-
-// 	return err
-// }
