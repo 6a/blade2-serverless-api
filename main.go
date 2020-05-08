adb61b10a023a84644ebdb16361a8a50305f0cf5
James Stanton <jestanton@outlook.com>
2020-05-08 02:56:34 +0100

    test main comments and cleanup

--- main.go
+++ main.go
@@ -1,7 +1,12 @@
+// Copyright 2020 James Einosuke Stanton. All rights reserved.
+// Use of this source code is governed by the MIT license
+// that can be found in the LICENSE.md file.
+
+// Package main implements interfaces for the lambda functions to be run, either through AWS
+// lambdas, or running locally.
 package main
 
 import (
-	"encoding/json"
 	"log"
 
 	"github.com/6a/blade-ii-api/internal/database"
@@ -9,28 +14,23 @@ import (
 	"github.com/aws/aws-lambda-go/events"
 )
 
+// This version of the main function is for running locally.
 func main() {
+
+	// Initialize the database package.
 	database.Init()
 
+	// Create a new empty AWS API gateway proxy request.
 	ev := events.APIGatewayProxyRequest{}
-	// ev.Headers = make(map[string]string)
-	ev.PathParameters = make(map[string]string, 1)
-	ev.PathParameters["pid"] = "bqnf9bu4h65c72kc033g"
-	// ev.QueryStringParameters = make(map[string]string, 2)
-	// ev.QueryStringParameters["from"] = "0"
-	// ev.QueryStringParameters["count"] = "10"
-	// ev.QueryStringParameters["pid"] = "bqnfd6e4h65c72kc0340"
 
+	// Perform a test call to one of the routes.
 	r, err := routes.GetMatchHistory(nil, ev)
 
+	// Check for an error.
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	data, err := json.MarshalIndent(r.Body, "", "  ")
-	if err != nil {
-		log.Print(r.Body)
-	} else {
-		log.Print(string(data))
-	}
+	// Print the returned result on success.
+	log.Print(r)
 }
