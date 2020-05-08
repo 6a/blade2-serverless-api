f663c61aa02fe6b266023c4532baad5904a9e7be
James Stanton <jestanton@outlook.com>
2020-05-08 05:06:48 +0100

    email and endpoints cleanup and comments

--- internal/email/email.go
+++ internal/email/email.go
@@ -1,12 +1,22 @@
+// Copyright 2020 James Einosuke Stanton. All rights reserved.
+// Use of this source code is governed by the MIT license
+// that can be found in the LICENSE.md file.
+
+// Package email provides functions for interacting with a mail server.
 package email
 
+// send is the internal send function.
 func send() (err error) {
+
+	// NOT IMPLEMENTED.
 
 	return err
 }
 
-// SendEmailConfirmation sends a an email to the specified address, for confirming an email address
+// SendEmailConfirmation sends a an email to the specified address, for confirming an email address.
 func SendEmailConfirmation(address string, handle string, token string) (err error) {
+
+	// NOT IMPLEMENTED.
 
 	return err
 }
--- internal/endpoints/update_mmr/update_mmr.go
+++ internal/endpoints/update_mmr/update_mmr.go
@@ -1,3 +1,9 @@
+// Copyright 2020 James Einosuke Stanton. All rights reserved.
+// Use of this source code is governed by the MIT license
+// that can be found in the LICENSE.md file.
+
+// Package main implements interfaces for the lambda functions to be run, either through AWS
+// lambdas, or running locally.
 package main
 
 import (
@@ -10,11 +16,16 @@ import (
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
+// functionWrapper is used so that this file can easily be copied and converted for use with another route.
 func functionWrapper(ctx context.Context, request events.APIGatewayProxyRequest) (r types.LambdaResponse, err error) {
 	return routes.UpdateMMR(ctx, request)
 }
 
 func main() {
+
+	// Initialize the database package.
 	database.Init()
+
+	// Start the lambda function handler.
 	lambda.Start(functionWrapper)
 }
